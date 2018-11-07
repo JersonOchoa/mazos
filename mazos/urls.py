a1de325ba4b4d200cabd9215611f5e5da453cc00
JersonOchoa <ochoarayo@hotmail.com>
2018-11-06 23:34:15 -0600

    Se agregaron los primeros templates, se arreglo un error de url

--- mazos/urls.py
+++ mazos/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     #url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
-    url(r'^mazos/nueva/$', views.ejercito_nuevo, name='ejercito_nuevo'),
+    #^mazos/nueva/$
+    url(r'^mazos/(?P<pk>[0-9]+)/edit/$', views.ejercito_nuevo, name='ejercito_nuevo'),
     ]
